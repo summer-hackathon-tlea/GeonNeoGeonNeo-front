5d0f51527472c13cb42b6ecb6cc10dbf802b931d
kimhyerin0909 <khyerin0909@gmail.com>
2022-07-14 02:20:54 +0900

    Feat - routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Main } from "./pages/OurWith/Main";
+import './styles/styles.css'
+
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ourwith" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
